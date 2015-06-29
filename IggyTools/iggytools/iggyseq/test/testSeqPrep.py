a06b59a8d1209b825616c736cc484e7eb9640c7f
Aaron Kitzmiller <aaronkitzmiller@RC-AKITZMILLER-1.local>
2015-06-29 11:53:58 -0400

    empty test

---
+++ IggyTools/iggytools/iggyseq/test/testSeqPrep.py
@@ -0,0 +1,30 @@
+"""
+Created on Jun 29, 2015
+Copyright (c) 2014
+Harvard FAS Research Computing
+All rights reserved.
+
+@author: aaronkitzmiller
+"""
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
