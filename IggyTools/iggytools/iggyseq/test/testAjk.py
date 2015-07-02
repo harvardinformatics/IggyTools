9f0bb98c639b033ce77d72774d2ecba0401afb23
akitzmiller <akitzmiller@g.harvard.edu>
2015-07-02 10:55:07 -0400

    Added IGGYPREFDIR to allow seqprep to specify an alternate directory

---
+++ IggyTools/iggytools/iggyseq/test/testAjk.py
@@ -0,0 +1,26 @@
+'''
+Created on Jul 2, 2015
+
+@author: akitzmiller
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
