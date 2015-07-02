0a130beb45f61607271a68cec1b0a720d9a91bf0
Michele Clamp <michele.clamp@gmail.com>
2015-07-02 17:10:46 -0400

    Added nextseq processing test

--- IggyTools/iggytools/iggyseq/test/testSeqPrep.py
+++ IggyTools/iggytools/iggyseq/test/testSeqPrep.py
@@ -7,8 +7,13 @@ All rights reserved.
 @author: aaronkitzmiller
 """
 
-import unittest
+from iggytools.utils.util                     import getUserHome
+from iggytools.pref.iggytools_PrefClass       import Iggytools_Preferences
+from iggytools.iggyseq.runClasses             import IlluminaNextGen, getSeqPref
+from iggytools.iggyseq.sampleSheetClasses     import BaseSampleSheet
 
+import unittest
+import os
 
 class Test(unittest.TestCase):
 
@@ -16,6 +21,21 @@ class Test(unittest.TestCase):
     def setUp(self):
         pass
 
+
+    def testNextSeqSeqPrep(self):
+
+      os.environ['IGGYPREFDIR']='./tests/data/iggytools_prefs/'
+      prefdir = os.environ.get('IGGYPREFDIR',None)
+
+      if prefdir is not None:
+        if not os.path.exists(prefdir):
+            raise Exception("IGGYPREFDIR %s does not exist" % prefdir)
+
+      runName  = "150527_NS500422_0126_AH2LC5AFXX"
+      seqpref  = getSeqPref(prefdir)
+
+      r        = IlluminaNextGen.getInstance(runName, pref = seqpref, verbose=True)
+      r.processRun()
 
     def tearDown(self):
         pass
