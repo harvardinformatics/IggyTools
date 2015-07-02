853d2f2c773f21d4012999be47d15451304fdc2d
Michele Clamp <michele.clamp@gmail.com>
2015-07-02 11:18:53 -0400

    Added sample sheet test stub

---
+++ IggyTools/iggytools/iggyseq/test/testSampleSheet.py
@@ -0,0 +1,29 @@
+"""
+Created on Jul 2, 2015
+Harvard FAS Informatics
+All rights reserved.
+
+@author: mclamp 
+"""
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
